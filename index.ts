96bba205519c8d1a031a75cc4f58a6374c3d0ecc
Joseph Sam <josephsamdev@gmail.com>
2023-10-30 11:31:10 +0000

    bun first look

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+   port: 4000,
+   fetch(req) {
+      const url = new URL(req.url);
+      if (url.pathname === "/") {
+         return new Response("Hello Bun! 🐰 from Deno and Node.js")
+      }
+
+      if (url.pathname === "/json") {
+         throw new Error("Error from Bun")
+      }
+
+      if (url.pathname === "/html") {
+         return new Response("<h1>Hello Bun! 🐰 from Deno and Node.js</h1>", {
+            headers: {
+               "content-type": "text/html"
+            }
+         })
+      }
+
+      if (url.pathname === "/greet") { 
+         return new Response(Bun.file("./greet.txt"))
+      }
+
+      return new Response("Not found", { status: 404 })
+   },
+   error(err) {
+      return new Response(err.message, { status: 500, headers: { "content-type": "text/plain" } })
+   }
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`)
